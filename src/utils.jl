068683ab1ac3bdcf9a9858531c9316560a85e5fe
Niall Moran <niall.moran@ichec.ie>
2021-03-29 12:05:41 +0100

    Change datatype of result

--- src/utils.jl
+++ src/utils.jl
@@ -104,12 +104,12 @@ end
 Function to reduce the dimension of the given tensor assuming the given hyper edge groups. For example a diagonal
 matrix will have a single hyper edge group with both indices [1, 2]
 
-```jldoctest
-julia> QXTn.reduce_tensor([[1, 0] [0, 2]], [[1, 2]])
-2-element Array{Int64,1}:
- 1
- 2
-```
+# ```jldoctest
+# julia> QXTn.reduce_tensor([[1, 0] [0, 2]], [[1, 2]])
+# 2-element Vector{Int64}:
+#  1
+#  2
+# ```
 """
 function reduce_tensor(A::AbstractArray{Elt, N}, hyper_index_groups::Array{Array{Int64, 1}, 1}) where {Elt, N}
     tensor_dims = size(A)
